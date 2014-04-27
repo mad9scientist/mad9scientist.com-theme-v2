700a3676ba78e675d6f8584cb235a6d64b6fe7af
Chris Holbrook <chris@madscitech.com>
2014-04-27 08:26:32 -0500

    Changed default page template to NOT have comment and changed no-comment page template to have comments. The reason being that most pages do not need or have comments by default.

--- page.php
+++ page.php
@@ -21,7 +21,7 @@
 	<h1><?php the_title(); ?></h1>
 	<?php the_content(); ?>
 </div>
-<?php comments_template( '', true ); ?>
+
 <?php endwhile; ?>
 
 <?php Starkers_Utilities::get_template_parts( array( 'parts/shared/footer','parts/shared/html-footer' ) ); ?>
