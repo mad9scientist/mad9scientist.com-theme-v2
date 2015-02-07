1556fb62bd341852576f3a95c64d988ee3e2444a
Chris Holbrook <chris@madscitech.com>
2015-02-06 20:47:30 -0600

    Updated Header, Footer and main content area to be no more than 1200px width. Resolves issue #1

--- parts/shared/footer.php
+++ parts/shared/footer.php
@@ -1,42 +1,42 @@
 	
-	<footer>
-	<div class="bio column">
-		<h5>About Me</h5>
-		<p>
-			<?php 
-				echo get_transient('bio-ftlinks');
-			?> 
-			<a href="/about/">Read More &raquo;</a>
-		</p>
-	</div>
-	<div class="ventures column">
-		<h5>My Ventures</h5>
-		<a href="//madscitech.com" class="mst">Mad Scientist Technologies</a>
-		<a href="//staticookie.com" class="sc">StatiCookie</a>
-		<a href="//jzscottmedia.com" class="jzsm">JZ Scott Media Production</a>
-	</div>
-	<div class="socialSites column">
-		<h5>Other Digitally Homes</h5>
-		<ul>
-			<?php 
-				echo get_transient('socialmedia-ftlinks');
-			?>
-		</ul>
-	</div>
-	<div class="friends column">
-		<h5>Some of Great Places</h5>
-		<ul>
-			<?php 
-				echo get_transient('shoutout-ftlinks');
-			?> 
-		</ul>
-	</div>
-	<div class="credits">
-		<div class="powered">
-			Proudly Powered by <a href="http://www.wordpress.org/">WordPress</a> and <a href="http://www.mediatemple.net/go/order/?refdom=mad9scientist.com">(mt) MediaTemple</a>.
+<footer>
+	<div class="innerContainer">
+		<div class="bio column">
+			<h5>About Me</h5>
+			<p>
+				<?php echo get_transient('bio-ftlinks'); ?> 
+				<a href="/about/">Read More &raquo;</a>
+			</p>
 		</div>
-		<div class="copyleft">
-			Copyright <?=date('Y')?> Christopher A Holbrook. Most Rights Reserved. <a href="/about/legal/">Legal?</a>
+		<div class="ventures column">
+			<h5>My Ventures</h5>
+			<a href="//madscitech.com" class="mst">Mad Scientist Technologies</a>
+			<a href="//staticookie.com" class="sc">StatiCookie</a>
+			<a href="//jzscottmedia.com" class="jzsm">JZ Scott Media Production</a>
+		</div>
+		<div class="socialSites column">
+			<h5>Other Digitally Homes</h5>
+			<ul>
+				<?php 
+					echo get_transient('socialmedia-ftlinks');
+				?>
+			</ul>
+		</div>
+		<div class="friends column">
+			<h5>Some of Great Places</h5>
+			<ul>
+				<?php 
+					echo get_transient('shoutout-ftlinks');
+				?> 
+			</ul>
+		</div>
+		<div class="credits">
+			<div class="powered">
+				Proudly Powered by <a href="http://www.wordpress.org/">WordPress</a> and <a href="http://www.mediatemple.net/go/order/?refdom=mad9scientist.com">(mt) MediaTemple</a>.
+			</div>
+			<div class="copyleft">
+				Copyright <?=date('Y')?> Christopher A Holbrook. Most Rights Reserved. <a href="/about/legal/">Legal?</a>
+			</div>
 		</div>
 	</div>
 </footer>
--- parts/shared/header.php
+++ parts/shared/header.php
@@ -5,33 +5,35 @@
 <noscript><p id="js-required"class="no-js-plz">Really, you don't have JavaScript Active!? Please turn on JavaScript to fully experience this site.</p></noscript>
 
 <header class="siteID" role="banner">
-	<a href="/job/" class="hireMe" title="Hire me via Mad Scientist Technologies!">Need Help, Hire Me!</a>
-	<div class="socialmedia">
-		<a href="/like/" class="fb social ir">Facebook</a>
-		<a href="/tweet/" class="twitter social ir">Twitter</a>
-		<a href="/git/" class="git social ir">GitHub</a>
+	<div class="innerContainer">
+		<a href="/job/" class="hireMe" title="Hire me via Mad Scientist Technologies!">Need Help, Hire Me!</a>
+		<div class="socialmedia">
+			<a href="/like/" class="fb social ir">Facebook</a>
+			<a href="/tweet/" class="twitter social ir">Twitter</a>
+			<a href="/git/" class="git social ir">GitHub</a>
+		</div>
+		<a href="<?php echo home_url(); ?>" class="logo">Mad9Scientist<span class="extendedTitle">.com</span> <span class="tagline"><?php bloginfo('description') ?></span></a>
+		<nav>
+			<div class="controller">
+				<a href="#nav">
+					<svg class="mobileNavIco" height="30" width="30">
+						<path stroke-width="5" stroke="#eee" d="M0,5 30,5"/>
+				  	 	<path stroke-width="5" stroke="#eee" d="M0,14 30,14"/>
+				    	<path stroke-width="5" stroke="#eee" d="M0,23 30,23"/>
+					</svg>
+				</a>
+			</div>
+			<div class="menuContents" role="navigation">
+				<ul>
+					<li><a href="/" class="index">Stuff I Wrote</a></li>
+					<li><a href="/portfolio/" class="portfolio">My Work</a></li>
+					<li><a href="/projects/" class="projects">Projects &amp; Code</a></li>
+					<li><a href="/contact/" class="contact">Reach Out</a></li>
+					<li><a href="/about/" class="about">Who?</a></li>
+				</ul>
+			</div>
+		</nav>
 	</div>
-	<a href="<?php echo home_url(); ?>" class="logo">Mad9Scientist<span class="extendedTitle">.com</span> <span class="tagline"><?php bloginfo('description') ?></span></a>
-	<nav>
-		<div class="controller">
-			<a href="#nav">
-				<svg class="mobileNavIco" height="30" width="30">
-					<path stroke-width="5" stroke="#eee" d="M0,5 30,5"/>
-			  	 	<path stroke-width="5" stroke="#eee" d="M0,14 30,14"/>
-			    	<path stroke-width="5" stroke="#eee" d="M0,23 30,23"/>
-				</svg>
-			</a>
-		</div>
-		<div class="menuContents" role="navigation">
-			<ul>
-				<li><a href="/" class="index">Stuff I Wrote</a></li>
-				<li><a href="/portfolio/" class="portfolio">My Work</a></li>
-				<li><a href="/projects/" class="projects">Projects &amp; Code</a></li>
-				<li><a href="/contact/" class="contact">Reach Out</a></li>
-				<li><a href="/about/" class="about">Who?</a></li>
-			</ul>
-		</div>
-	</nav>
 </header>
 
 <aside class="Intro">
