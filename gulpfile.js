eecd66f1f88c6aac93d5276f57cce00ed2fb28ea
Chris Holbrook <chris@madscitech.com>
2015-03-05 04:02:53 -0600

    Add gulp task to minify Javascript file. Add new directory for minify javascript.

--- gulpfile.js
+++ gulpfile.js
@@ -2,6 +2,7 @@ var gulp = require('gulp'),
 	watch = require('gulp-watch'),
 	autoprefixer = require('gulp-autoprefixer'),
 	sourcemap = require('gulp-sourcemaps'),
+	uglify = require('gulp-uglify'),
 	sass = require('gulp-ruby-sass');
 
 gulp.task('sass', function(){
@@ -14,14 +15,21 @@ gulp.task('sass', function(){
 gulp.task('prefix', function(){
 	return gulp.src('.gulp-cache/style.css')
 				.pipe(autoprefixer({
-					browsers: ['> 1%', 'last 3 versions', 'Firefox ESR', 'Opera 12.1', 'ie 9']					
+					browsers: ['> 1%', 'last 3 versions', 'Firefox ESR', 'Opera 12.1', 'ie 9']
 				}))
 				.pipe(gulp.dest('./'));
+});
+
+gulp.task('minifyJS', function(){
+	return gulp.src('js/core.mad9scientist.com.js')
+				.pipe(uglify())
+				.pipe(gulp.dest('js/dist'))
 });
 
 gulp.task('watch', function(){
 	gulp.watch('sass/**/*.scss', ['sass']);
 	gulp.watch('.gulp-cache/**/*.css', ['prefix']);
+	gulp.watch('js/core.mad9scientist.com.js', ['minifyJS']);
 });
 
 gulp.task('default', ['watch']);
