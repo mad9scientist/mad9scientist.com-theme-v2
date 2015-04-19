5a05f35391d96ca39eaf75485c92aaa3362e2de9
Chris Holbrook <chris@madscitech.com>
2015-04-19 03:21:34 -0500

    Updated Gulp Components and Added notification function to gulp tasks.

--- gulpfile.js
+++ gulpfile.js
@@ -3,6 +3,7 @@ var gulp = require('gulp'),
 	autoprefixer = require('gulp-autoprefixer'),
 	sourcemap = require('gulp-sourcemaps'),
 	uglify = require('gulp-uglify'),
+	notify = require('gulp-notify'),
 	sass = require('gulp-ruby-sass');
 
 gulp.task('sass', function(){
@@ -10,20 +11,35 @@ gulp.task('sass', function(){
 			.on('error', function(err) {
 				console.error('Error!', err.message);
 			})
-			.pipe(gulp.dest('.gulp-cache'));
+			.pipe(gulp.dest('.gulp-cache'))
+			.pipe(notify({
+				title: "Stylesheets",
+				message: "SASS Processing Completed",
+				sound: true
+			}));
 });
 gulp.task('prefix', function(){
-	return gulp.src('.gulp-cache/style.css')
+	return gulp.src('./.gulp-cache/style.css')
 				.pipe(autoprefixer({
 					browsers: ['> 1%', 'last 3 versions', 'Firefox ESR', 'Opera 12.1', 'ie 9']
 				}))
-				.pipe(gulp.dest('./'));
+				.pipe(gulp.dest('./'))
+				.pipe(notify({
+					title: "Prefixing",
+					message: "AutoPrefixer Processing Completed",
+					sound: true
+				}));
 });
 
 gulp.task('minifyJS', function(){
 	return gulp.src('js/core.mad9scientist.com.js')
 				.pipe(uglify())
 				.pipe(gulp.dest('js/dist'))
+				.pipe(notify({
+					title: "Scripts",
+					message: "Javascript Minified",
+					sound: true
+				}));
 });
 
 gulp.task('watch', function(){
