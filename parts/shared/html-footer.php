29822b81a6549bfad37adae4d5e3d88b5cb1b656
Chris Holbrook <chris@madscitech.com>
2016-05-23 12:46:51 -0500

    Removed Pwik Analytic Code

--- parts/shared/html-footer.php
+++ parts/shared/html-footer.php
@@ -12,20 +12,6 @@
 		g.src=('https:'==location.protocol?'//ssl':'//www')+'.google-analytics.com/ga.js';
 		s.parentNode.insertBefore(g,s)}(document,'script'));
     </script>
-    
-    <!-- Piwik -->
-    <script type="text/javascript">
-      var _paq = _paq || [];
-      _paq.push(['trackPageView']);
-      _paq.push(['enableLinkTracking']);
-      (function() {
-        var u="//services.madscitech.com/analytics/";
-        _paq.push(['setTrackerUrl', u+'piwik.php']);
-        _paq.push(['setSiteId', 2]);
-        var d=document, g=d.createElement('script'), s=d.getElementsByTagName('script')[0];
-        g.type='text/javascript'; g.async=true; g.defer=true; g.src=u+'piwik.js'; s.parentNode.insertBefore(g,s);
-      })();
-    </script>
 
 	</body>
 </html>
