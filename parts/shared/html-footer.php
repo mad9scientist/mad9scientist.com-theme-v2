c933b2cd9c56ec7ae9c151cb25490ff67fa3215b
Chris Holbrook <chris@madscitech.com>
2023-06-26 13:45:00 -0500

    Update html-footer.php
    Installed new code for Migration to Google Analytics 4. See issue #5.

--- parts/shared/html-footer.php
+++ parts/shared/html-footer.php
@@ -5,13 +5,22 @@
 	<script src="<?php echo get_stylesheet_directory_uri().'/js/vendor/respond.min.js';?>"></script>
 	<![endif]-->
 
-	<!-- Google Analytics -->
+	<!-- Google tag (gtag.js) -->
+	<script async src="https://www.googletagmanager.com/gtag/js?id=G-FWGRMHHN7P"></script>
+	<script>
+		window.dataLayer = window.dataLayer || [];
+		function gtag(){dataLayer.push(arguments);}
+		gtag('js', new Date());
+		gtag('config', 'G-FWGRMHHN7P');
+	</script>
+
+	<!-- Google Analytics (Old Remove Sept 2023) -->
 	<script>
 		var _gaq=[['_setAccount','UA-16682171-1'],['_trackPageview']];
 		(function(d,t){var g=d.createElement(t),s=d.getElementsByTagName(t)[0];
 		g.src=('https:'==location.protocol?'//ssl':'//www')+'.google-analytics.com/ga.js';
 		s.parentNode.insertBefore(g,s)}(document,'script'));
-    </script>
+	</script>
 
 	</body>
 </html>
